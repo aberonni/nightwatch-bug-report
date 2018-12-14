e626f3ce41e1f49df734b00c9ea2f2df8345334a
Domenico Gemoli <domenicogemoli@gmail.com>
2018-12-14 11:11:37 +0100

    lint

--- tests/1940/nightwatch.conf.js
+++ tests/1940/nightwatch.conf.js
@@ -3,11 +3,11 @@ const path = require('path');
 const baseConfig = require('../../nightwatch.conf.base');
 
 module.exports = {
-    ...baseConfig,
-    src_folders: [path.join(__dirname, 'src')],
-    test_settings: {
-        default: {
-            default_path_prefix : "",
-        }
+  ...baseConfig,
+  src_folders: [path.join(__dirname, 'src')],
+  test_settings: {
+    default: {
+      default_path_prefix : '',
     }
+  }
 };
--- tests/1940/src/test.js
+++ tests/1940/src/test.js
@@ -1,23 +1,23 @@
 module.exports = {
-    // tags: ['issue 1940'],
-    'part one - proper url': function(client) {
-        client
-            .url('http://localhost:8080')
-            .getText('li[class="speed"] div[class="current-speed"]', ({value}) => {
-                client.assert.equal(value, 10, 'speed successfully read');
-            });
+  // tags: ['issue 1940'],
+  'part one - proper url': function(client) {
+    client
+      .url('http://localhost:8080')
+      .getText('li[class="speed"] div[class="current-speed"]', ({value}) => {
+        client.assert.equal(value, 10, 'speed successfully read');
+      });
 
-    },
+  },
 
-    // 'part two - wrong url': function(client) {
-    //     client
-    //         .url('http://github.com/nightwatchjs/nightwatch')
-    //         .getText('li[class="speed"] div[class="current-speed"]', ({value}) => {
-    //             client.assert.equal(value, 10, 'speed successfully read');
-    //         });
-    // },
+  // 'part two - wrong url': function(client) {
+  //     client
+  //         .url('http://github.com/nightwatchjs/nightwatch')
+  //         .getText('li[class="speed"] div[class="current-speed"]', ({value}) => {
+  //             client.assert.equal(value, 10, 'speed successfully read');
+  //         });
+  // },
 
-    after(client) {
-        client.end();
-    }
+  after(client) {
+    client.end();
+  }
 };
