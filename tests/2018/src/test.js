f5ee2392303ad15d2f5e231b24f7700267682be4
Thaddeus Stewart <thad@truqcapp.com>
2019-02-06 11:10:08 -0500

    flush out test

--- tests/2018/src/test.js
+++ tests/2018/src/test.js
@@ -1,11 +1,20 @@
 module.exports = {
-  'Demo test': function (browser) {
-    const dataForCommand = 'a string';
-    const page = browser.page.myPage();
-
-    page
-      .navigate(browser.launchUrl)
+  before: function (browser) {
+    browser.url(browser.launchUrl);
+  },
+  'Passing without page object provding Array': function (browser) {
+    const dataForCommand = ['foo'];
+    browser.customCommand(dataForCommand);
+  },
+  'Passing with a sring': function (browser) {
+    const dataForCommand = 'foo';
+    browser.page.myPage()
+      .customCommand(dataForCommand);
+  },
+  'Failing with an Array': function (browser) {
+    const dataForCommand = ['foo']; // will fail using object: { foo: 'bar' }
+    browser.page.myPage()
       .customCommand(dataForCommand)
       .api.end();
-  }
+  },
 };
